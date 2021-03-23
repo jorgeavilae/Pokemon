04b366551136bb7ef899afadb89fa9db42924bc5
Jorge <jorgeavilae@gmail.com>
2021-03-23 19:19:04 +0100

    Provide back navigation

--- app/src/main/java/com/atsistemas/pokemon/main_activity/MainActivity.kt
+++ app/src/main/java/com/atsistemas/pokemon/main_activity/MainActivity.kt
@@ -48,4 +48,8 @@ class MainActivity : BaseActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return findNavController(R.id.nav_host_fragment).navigateUp()
+    }
 }
